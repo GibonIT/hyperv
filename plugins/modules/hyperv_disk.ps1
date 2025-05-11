99972c20f70c46e7a7ac71635eea89ae33fa2fdc
Leos Marek <93908808+GibonIT@users.noreply.github.com>
2025-05-11 06:59:35 +0200

    Create hyperv_disk.ps1

---
+++ plugins/modules/hyperv_disk.ps1
@@ -0,0 +1,58 @@
+# Copyright: (c) 2025, Leos Marek (@Gibonnn)
+# GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
+
+#AnsibleRequires -CSharpUtil Ansible.Basic
+
+$ErrorActionPreference = "Stop"
+
+$spec = @{
+  options = @{
+    source_vhdx_path = @{ type = "str"; required = $true }
+    destination_vhdx_path = @{ type = "str"; required = $true }
+    type = @{ type = "str"; choices = @("fixed", "dynamic", "differencing"); default = "dynamic" }
+    delete_source = @{ type = "bool"; default = $false }
+  }
+  supports_check_mode = $true
+}
+
+$module = [Ansible.Basic.AnsibleModule]::Create($args, $spec)
+
+$sourceVhdxPath = $module.Params.source_vhdx_path
+$destinationVhdxPath = $module.Params.destination_vhdx_path
+$deleteSource = $module.Params.delete_source
+$type = $module.Params.type
+
+$result = @{
+  changed = $false
+}
+
+Function Clone-VHDX {
+  if (-Not (Test-Path -Path $sourceVhdxPath)) {
+    $module.FailJson("Source VHDX file does not exist: $sourceVhdxPath")
+  }
+
+  if (Test-Path -Path $destinationVhdxPath) {
+    $module.FailJson("Destination VHDX file already exists: $destinationVhdxPath")
+  }
+
+  if ($module.CheckMode) {
+    $module.result.desired_action = "Clone VHDX from $sourceVhdxPath to $destinationVhdxPath"
+    $module.result.changed = $true
+  } else {
+    try {
+      $results = Convert-VHD -Path $sourceVhdxPath -DestinationPath $destinationVhdxPath -VHDType $type -DeleteSource:$deleteSource -Passthru
+      $module.result.vhdx_info = $results
+      $module.result.changed = $true
+    } Catch {
+      $module.FailJson($_)
+    }
+  }
+}
+
+Try {
+  Clone-VHDX
+} Catch {
+  $module.FailJson($_)
+}
+
+$module.ExitJson()
